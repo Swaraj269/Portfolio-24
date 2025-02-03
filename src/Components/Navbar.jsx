f61db0f276fba61d97d0d8e9d9432bcdafc392d8
swaraj singh <swarajsingh494@gmail.com>
2025-02-03 15:03:38 +0530

    change cv

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap";
 import React from "react";
 import { TfiDownload } from "react-icons/tfi";
-import resume from "../assets/SwarajSingh CV.pdf";
+import resume from "../assets/Swaraj-Singh-CV.pdf";
 function Navbar() {
   const mouseenter = () => {
     gsap.to(".leftnavtexts h1", {
